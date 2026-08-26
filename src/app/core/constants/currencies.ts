c9809828cc74376b8f6775518b9e3bd01c779580
Lautaro <lautibonet@gmail.com>
2026-08-26 15:28:23 +0200

    Added ARS to supported currencies

--- src/app/core/constants/currencies.ts
+++ src/app/core/constants/currencies.ts
@@ -1,5 +1,5 @@
 export const SUPPORTED_CURRENCIES = [
-  'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK',
+  'ARS', 'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK',
   'EUR', 'GBP', 'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'ISK',
   'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN',
   'RON', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR',
